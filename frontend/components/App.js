35bb9e038052985b4d040a8a476b844c8dcb6ab5
Fatimah Ismael <fatimahismael@yahoo.com>
2022-12-06 11:03:28 -0800

    start of todo

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
   render() {
     return (
-      <div>
-        Todo App
+      <div className='App'>
+       <h2>Todo App</h2> 
+       <ul>
+        <li>Complete module 1</li>
+        <li>Nasreen's play registration</li>
+        <li>Reading with Anisa</li>
+       </ul>
+       <form>
+        <input></input>
+        <button>Add</button>
+       </form>
+       <button>Clear</button>
       </div>
     )
   }
