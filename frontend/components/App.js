09886b73b58c554b74e64e982d41d17078b6cf7f
Fatimah Ismael <fatimahismael@yahoo.com>
2022-12-06 16:30:08 -0800

    completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Todo from './Todo';
 import TodoList from './TodoList';
+import Form from './Form'
+
 export default class App extends React.Component {
   constructor() {
     super();
@@ -19,6 +20,56 @@ export default class App extends React.Component {
         
       ]
     }
+  
+  }
+  handleAdd=(name)=>{
+    //1.setState
+    //2.change todos
+    //3.make a copy todos
+    //4. add a new todo to the end
+    
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      ...this.state, 
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+  handleToggle = ()=> {
+    //1. setState
+    //2. change todos
+    //3. find the todo we clicked on
+    //4. flip the value of completed for that todo
+    //5. keep all other todos the same
+    const clickedId = 1528817084358;
+    this.setState({
+      ...this.setState,
+      todos: this.state.todos.map(todo=> {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo;
+      })
+    })
+  }
+
+  handleClear = ()=> {
+    //1. setState
+    //2. loop through all todos
+    //3. remover all todos that have completed ===true
+    //4. save filered todos to state
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo =>{
+        return (todo.completed === false);
+      })
+    })
   }
   render() {
     const { todos } =this.state;
@@ -27,12 +78,9 @@ export default class App extends React.Component {
     return (
       <div>
        <h2>Todo App</h2> 
-       <TodoList todos={todos}/>
-       <form>
-        <input></input>
-        <button>Add</button>
-       </form>
-       <button>Clear</button>
+       <TodoList handleToggle={this.handleToggle} todos={todos}/>
+       <Form handleAdd={this.handleAdd}  />
+       <button onClick={this.handleClear}>Clear</button>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+  handleSubmit = (e)=> {
+    e.preventDefault();
+    this.props.handleAdd(this.state.input);
+    //console.log('submit');
+  }
+  handleChange = (e)=> {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    });
+  }
   render() {
     return (
-      <div>
-        Form
-      </div>
+     <form>
+        <input onChange={this.handleChange}/>
+        <button onClick= {this.handleSubmit}>Add</button>
+       </form>
     )
   }
 }
